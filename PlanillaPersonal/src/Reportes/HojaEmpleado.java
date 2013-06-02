53d2f718015e2aaefe9435d9a6a46878fe517159
SixRosales <rcsixto@hotmail.com>
2013-06-02 11:42:39 -0600

    Extension del reporte de Empleado

--- PlanillaPersonal/src/Reportes/HojaEmpleado.java
+++ PlanillaPersonal/src/Reportes/HojaEmpleado.java
@@ -1,5 +1,13 @@
-package PlanillaPersonal;
+package Reportes;
 
-Class HojaEmpleado{
+class HojaEmpleado {
 
+    private String Id_Empleado;
+    private String Nombre;
+    private String Apellido;
+    
+    
+    public void setName(String name){
+        this.Nombre = name;
+    }
 }
